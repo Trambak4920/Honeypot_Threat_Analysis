339d2cae8e22c81f0b86b301b07405220ae9378d
Neel2k5 <s.dutta2k5@gmail.com>
2025-02-18 10:16:11 +0530

    Updated the way api processes requests

--- Interface/Backend/api-server-src/api-server.py
+++ Interface/Backend/api-server-src/api-server.py
@@ -1,12 +1,13 @@
-from flask import Flask,jsonify,render_template
+from flask import Flask,jsonify,render_template,request
 import pymongo
 from pymongo import MongoClient
 import os
+from flask_cors import CORS
 
 #Constants are to be configred via ENv vars in later development stages
 #Database integration constants
-MONGO_URL=str(os.getenv("MONGO_SERVER_URL"))
-
+#MONGO_URL=str(os.getenv("MONGO_SERVER_URL"))# production
+MONGO_URL="mongodb://localhost:27017"#testing
 #SERVER constants
 HOST = "127.0.0.1"
 PORT = "7500"
@@ -14,6 +15,7 @@ PORT = "7500"
 
 
 server_obj = Flask(__name__)
+CORS(server_obj)# Allowing CORS for all routes
 
 #Routes
 @server_obj.route("/")
@@ -21,24 +23,51 @@ def Homepage():
 	return render_template("home.html")
 
 # READ Routes
-@server_obj.route("/api/read/<string:db>/<string:col>",methods =["GET"])
-def getDBCOL(db,col):
+@server_obj.route("/api/read/all",methods =["GET"])
+def getDBCOL():
+	
+	db=request.args.get("db")
+	col=request.args.get("col")
+	
+	response={
+	"status":None,
+	"data":None
+	}
+	
 	mongo_client = MongoClient(MONGO_URL)
 	mongo_db_obj = mongo_client[db]
 	mongo_col_obj = mongo_db_obj[col]
-	data = list(mongo_col_obj.find({},{"_id":0}))
-	if(not data):
-		return jsonify({"error":"NOT FOUND"})
-	return jsonify(data)
-@server_obj.route("/api/read/<string:db>/<string:col>/<string:key>/<string:value>",methods=["GET"])
-def getKV(db,col,key,value):
+	
+	response["data"] = list(mongo_col_obj.find({},{"_id":0}))
+	
+	if(not response["data"]):
+		response["status"]=404
+	else:
+		response["status"]=200
+	return jsonify(response)
+	
+@server_obj.route("/api/read/kv",methods=["GET"])
+def getKV():
+	
+	db=request.args.get("db")
+	col=request.args.get("col")
+	key=request.args.get("key")
+	value=request.args.get("value")
+	response={
+	"status":None,
+	"data":None
+	}
 	mongo_client = MongoClient(MONGO_URL)
 	mongo_db_obj = mongo_client[db]
 	mongo_col_obj = mongo_db_obj[col]
-	data = list(mongo_col_obj.find_one({key:value},{"_id":0}))
-	if(not data):
-		return jsonify({"error":"NOT FOUND"})
-	return jsonify(data)
+	
+	response["data"] = list(mongo_col_obj.find_one({key:value},{"_id":0}))
+	
+	if(not response["data"]):
+		response["status"]=404
+	else:
+		response["status"]=200
+	return jsonify(response)
 	
 
 
